1e45fc0ce7c2ef1591ca422d4c5a6bdd4c6c48c3
TabbyMichael <120185762+TabbyMichael@users.noreply.github.com>
2025-04-22 17:38:36 +0300

    Ios

--- lib/screens/increment_screen.dart
+++ lib/screens/increment_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:toropal_clone/providers.dart';
+import 'package:toropal_clone/managing%20states/providers.dart';
 
 class IncrementScreen extends ConsumerStatefulWidget {
   const IncrementScreen({super.key});
